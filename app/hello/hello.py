304c194b25166b2065d7275567962e0016ba5224
kiran.haridas <kiran.haridas@ey.com>
2019-02-19 00:46:47 -0800

    Migrate Changes to DB

--- app/hello/hello.py
+++ app/hello/hello.py
@@ -6,6 +6,7 @@ import pytz
 
 from . import hello
 from ..models import DatabaseTables,db
+from random import randint
 
 
 
@@ -14,18 +15,26 @@ from ..models import DatabaseTables,db
 def hello_world():
     message = "Hello World!  {}".format(get_pst_time())
     image_src = "https://s3.amazonaws.com/kiran-test-2/cruiser80.jpg"
-
+    random_id = randint(0, 10000)
     # Database
     db_string = DatabaseTables.query.all
-    me = DatabaseTables(2, 'Kiran Haridas')
+    me = DatabaseTables(id=random_id, name='Kiran')
     db.session.add(me)
     db.session.commit()
+
+    row = db.session.query(DatabaseTables).filter(
+          DatabaseTables.name == 'Kiran').first()
+    print('original:', row.name, row.id)
+
+    # query= db.session.query(DatabaseTables.name).order_by(DatabaseTables.id)
+    # print('original:', query.key, query.val)
+
 
 
     return render_template("hello.html",
                            src_hello=message,
                            image_name=image_src,
-                           db_message=db_string
+                           db_message=row
                            )
 def get_pst_time():
     # date_format = '%m/%d/%Y %H:%M:%S %Z'
