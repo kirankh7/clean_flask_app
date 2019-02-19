8b3a156d1aa5cf425dab559813224962dc44d878
kiran.haridas <kiran.haridas@ey.com>
2019-02-19 00:48:33 -0800

    DB_URL change

--- app/config.py
+++ app/config.py
@@ -10,10 +10,10 @@ class Config:
     # DB Connection settings
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "someshabasednumberlkira"
-    SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL') or \
-                              'sqlite:///' + os.path.join(basedir, 'example.db')
+    # SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL') or \
+    #                           'sqlite:///' + os.path.join(basedir, 'example.db')
 
-    # SQLALCHEMY_DATABASE_URI = "mysql://" + config.get('DB_USERNAME') + ':' + config.get('DB_PASSWORD') + '@' + config.get('DB_ENDPOINT') + '/' + config.get('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = "mysql://" + config.get('DB_USERNAME') + ':' + config.get('DB_PASSWORD') + '@' + config.get('DB_ENDPOINT') + '/' + config.get('DB_NAME')
     # DB_ENDPOINT = config.get('DB_ENDPOINT')
     # DB_NAME = config.get('DB_NAME')
     # DB_USERNAME = config.get('DB_USERNAME')
